504da8840da78a73376a0a9313cbd812e1f0cdd7
Shivashish Das <dasshivashish11@gmail.com>
2025-04-05 13:18:10 +0530

    add support for decoders

--- utils/TableGen.cpp
+++ utils/TableGen.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
-
+#include <map>
+#include <string>
 #define	FILE_INACCESSIBLE 1
 
 int main(int argc, const char** argv) {
@@ -7,13 +8,20 @@ int main(int argc, const char** argv) {
 	file.open(argv[1]);
 	if (!file.is_open()) 
 		return FILE_INACCESSIBLE;
-
+	std::map<int, std::string> bltns;
+	for (int i = 0; i < 4; i++) {
+		bltns[i] = "decode_eb_gb";
+	}
 	file << "#pragma once" << std::endl;
 	file << "struct DecoderState;" << std::endl;
 	file << "typedef int (*DecodeFunc)(DecoderState*);" << std::endl;
 	file << "const DecodeFunc Decoders[] = {" << std::endl;
 	for (int i = 0; i < 256; i++) {
-		file << "nullptr," << std::endl;
+		auto fn = bltns.find(i);
+		if (fn != bltns.end()) 
+			file << fn->second << "," << std::endl;
+		else 
+			file << "nullptr," << std::endl;
 	}
 	file << "};" << std::endl;
 	file.close();
