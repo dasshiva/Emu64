7c9127f38cac3bfc1330fcb80ce6603b8fe18d45
Shivashish Das <dasshivashish11@gmail.com>
2025-04-09 10:24:00 +0530

    add support for regs

--- utils/TableGen.cpp
+++ utils/TableGen.cpp
@@ -1,137 +1,170 @@
 #include <cstdint>
 #include <fstream>
+#include <iostream>
 #include <map>
 #include <ostream>
 #include <string>
-#include <iostream>
+#include <cstring>
 
 extern "C" {
 #include "cfg/cfg_parse.h"
 }
 
-#define	FILE_INACCESSIBLE 1
+#define FILE_INACCESSIBLE 1
 
-/*void addBuiltin(std::map<int, std::string>& bltns, std::string toappend, int fst){
-	bltns[fst] = toappend;
+/*void addBuiltin(std::map<int, std::string>& bltns, std::string toappend, int
+fst){ bltns[fst] = toappend;
 }
 
 template<typename... Args>
-void addBuiltin(std::map<int, std::string>& bltns, std::string toappend, int fst, Args... n) {
-	bltns[fst] = toappend;
-	addBuiltin(bltns, toappend, n...);
+void addBuiltin(std::map<int, std::string>& bltns, std::string toappend, int
+fst, Args... n) { bltns[fst] = toappend; addBuiltin(bltns, toappend, n...);
 }
 
 int main(int argc, const char** argv) {
-	if (argc != 2) 
-		return 1;
-	std::ofstream file;
-	file.open(argv[1]);
-	if (!file.is_open()) 
-		return FILE_INACCESSIBLE;
+        if (argc != 2)
+                return 1;
+        std::ofstream file;
+        file.open(argv[1]);
+        if (!file.is_open())
+                return FILE_INACCESSIBLE;
 
-	file << "struct DecoderState;\n";
-	file << "struct DecodedInstruction;\n";
+        file << "struct DecoderState;\n";
+        file << "struct DecodedInstruction;\n";
 
-	// Add builtin decoder functions here as needed
-	std::map<int, std::string> bltns;
-	addBuiltin<int>(bltns, "decode_eb_gb", 0, 0x10, 0x20, 0x30);
+        // Add builtin decoder functions here as needed
+        std::map<int, std::string> bltns;
+        addBuiltin<int>(bltns, "decode_eb_gb", 0, 0x10, 0x20, 0x30);
 
-	file << "// auto generated DO NOT EDIT MANUALLY" << std::endl;
-	file << "// Edit utils/TableGen.cpp if you need changes here" 
-		<< std::endl;
+        file << "// auto generated DO NOT EDIT MANUALLY" << std::endl;
+        file << "// Edit utils/TableGen.cpp if you need changes here"
+                << std::endl;
 
-	// Some duplicate function prototypes will be created
-	// but most compilers do not care about some extra 
-	// function signature declarations
-	for (auto it = bltns.begin(); it != bltns.end(); it++) {
-		file << "int " << it->second << "(struct DecoderState*, struct DecodedInstruction*);\n";
-	}
+        // Some duplicate function prototypes will be created
+        // but most compilers do not care about some extra
+        // function signature declarations
+        for (auto it = bltns.begin(); it != bltns.end(); it++) {
+                file << "int " << it->second << "(struct DecoderState*, struct
+DecodedInstruction*);\n";
+        }
 
-	// Declare fundamental types
-	file << "typedef int (*DecodeFunc)(struct DecoderState*, struct DecodedInstruction*);" << std::endl;
-	file << "const DecodeFunc Decoders[] = {" << std::endl;
-	for (int i = 0; i < 256; i++) {
-		auto fn = bltns.find(i);
-		if (fn != bltns.end()) 
-			file << fn->second << "," << std::endl;
-		else 
-			file << "((void*)0)," << std::endl;
-	}
-	
-	file << "};" << std::endl;
+        // Declare fundamental types
+        file << "typedef int (*DecodeFunc)(struct DecoderState*, struct
+DecodedInstruction*);" << std::endl; file << "const DecodeFunc Decoders[] = {"
+<< std::endl; for (int i = 0; i < 256; i++) { auto fn = bltns.find(i); if (fn !=
+bltns.end()) file << fn->second << "," << std::endl; else file << "((void*)0),"
+<< std::endl;
+        }
 
-	std::map<int, std::string> opcodes;
-	// Add opcodes here as needed
-	opcodes[0] = "ADD_EB_GB";
-	opcodes[0x10] = "ADC_EB_GB";
-	opcodes[0x20] = "AND_EB_GB"; 
-	opcodes[0x30] = "XOR_EB_GB";
-	for (auto it = opcodes.begin(); it != opcodes.end(); it++) {
-		file << "#define " << it->second << " (" << it->first << ")\n";
-	}
+        file << "};" << std::endl;
 
-	file << "const unsigned short OpcodeMap1[16][16] = {" << std::endl;
-	for (uint8_t i = 0; i < 16; i++) {
-		file << "{";
-		for (uint8_t j = 0; j < 16; j++) {
+        std::map<int, std::string> opcodes;
+        // Add opcodes here as needed
+        opcodes[0] = "ADD_EB_GB";
+        opcodes[0x10] = "ADC_EB_GB";
+        opcodes[0x20] = "AND_EB_GB";
+        opcodes[0x30] = "XOR_EB_GB";
+        for (auto it = opcodes.begin(); it != opcodes.end(); it++) {
+                file << "#define " << it->second << " (" << it->first << ")\n";
+        }
 
-			auto op = opcodes.find((i << 4) | j);
-			if (op != opcodes.end()) 
-				file << op->second;
-			else
-				file << "0xFFFF";
-			if (j != 15) 
-				file << ",";
-		}
+        file << "const unsigned short OpcodeMap1[16][16] = {" << std::endl;
+        for (uint8_t i = 0; i < 16; i++) {
+                file << "{";
+                for (uint8_t j = 0; j < 16; j++) {
 
-		file << "}";
-		if (i != 15) 
-			file << ",";
-		file << "\n";
-	}
-	file << "};\n";
-	file.close();
-	return 0;
+                        auto op = opcodes.find((i << 4) | j);
+                        if (op != opcodes.end())
+                                file << op->second;
+                        else
+                                file << "0xFFFF";
+                        if (j != 15)
+                                file << ",";
+                }
+
+                file << "}";
+                if (i != 15)
+                        file << ",";
+                file << "\n";
+        }
+        file << "};\n";
+        file.close();
+        return 0;
 } */
 
-int main(int argc, const char** argv) {
+int processRegs(Value* val, std::ofstream& hfile) {
+    Vector *bregs = val->Array;
+    for (uint64_t i = 0; i < bregs->Length; i++) {
+        PrimitiveValue *pv = GetElement(bregs, i);
+        if (pv->Type != STRING_TYPE) {
+            std::cout << "All Values in *Regs must be strings\n";
+            return -1;
+        }
+
+        hfile << "#define REG_" << pv->String << " (" << i << ")" << "\n";
+    }
+    hfile << std::endl;
+
+    return 0;
+}
+
+int main(int argc, const char **argv) {
     if (argc != 3) {
-	std::cout << "Usage: " << argv[0] << " [CONF].cfg [OUTPUT].h\n";
-	return 1;
+        std::cout << "Usage: " << argv[0] << " [CONF].cfg [OUTPUT].h\n";
+        return 1;
     }
 
-    Config* config;
+    Config *config;
     int status = ParseConfig(argv[1], &config);
     if (status < 0) {
-	std::cout << "ERROR: " << ErrToString(status) << "\n";
-	return 1;
+        std::cout << "ERROR: " << ErrToString(status) << "\n";
+        return 1;
     }
 
-    std::ofstream file;
-    file.open(argv[2]);
-    if (!file.is_open()) {
-	std::cout << "Failed to access output file " << argv[2] << "\n";
-	return 1;
+    // hfile is the header, sfile is the source
+    std::ofstream hfile, sfile;
+    hfile.open(std::string(argv[2]) + ".h");
+    sfile.open(std::string(argv[2]) + ".c");
+
+    if (!hfile.is_open()) {
+        std::cout << "Failed to access output file " << argv[2] << ".h \n";
+        return 1;
     }
 
-    Value* bvec = FindValue(config, ARRAY_TYPE, "ByteRegs");
-    if (!bvec) {
-	std::cout << "Required Key 'ByteRegs' not found" << "\n";
-	return 1;
+    if (!sfile.is_open()) {
+        std::cout << "Failed to access output file " << argv[2] << ".c \n";
+        return 1;
     }
 
-    Vector* bregs = bvec->Array;
-    for (uint64_t i = 0; i < bregs->Length; i++) {
-	PrimitiveValue* pv = GetElement(bregs, i);
-	if (pv->Type != STRING_TYPE) {
-	    std::cout << "All Values in ByteRegs must be strings\n";
-	    return 1;
-	}
+    hfile << "// AUTO GENERATED - DO NOT EDIT MANUALLY\n";
+    hfile << "// Consider editing utils/file.cfg or utils/TableGen.cpp instead\n";
+    hfile << "#pragma once\n";
 
-	file << "#define REG_" << pv->String << " (" << i << ")" << "\n";
+    sfile << "// AUTO GENERATED - DO NOT EDIT MANUALLY\n";
+    sfile << "// Consider editing utils/file.cfg or utils/TableGen.cpp instead\n";
+    sfile << "#include \"" << std::string(argv[2]) + ".h" << "\" \n";
+
+    ConfigEntry *ce = config->List;
+    for (uint64_t idx = 0; idx < config->Entries; idx++) {
+        uint64_t len = std::strlen(ce->Key);
+        if (len > 4)  { // 4 = strlen("Regs")
+            if (ce->Key[len-4] == 'R' && ce->Key[len-3] == 'e' &&
+                    ce->Key[len-2] == 'g' && ce->Key[len-1] == 's') {
+                if (ce->Type != ARRAY_TYPE) {
+                    std::cout << "All *Regs keys must be arrays\n";
+                    return -1;
+                }
+
+                if (processRegs(ce->Value, hfile) < 0) 
+                    return 1;
+            }
+        }
+
+        ce = ce->Next;
     }
 
-    file.close();
+    hfile.close();
+    sfile.close();
     FreeConfig(config);
     return 0;
 }
